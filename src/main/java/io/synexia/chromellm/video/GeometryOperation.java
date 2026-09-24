814b4d3ef4cda5d6ae321d62bb9c2f572731f1f1
Hitesh Soliwal <hsoliwal@gmail.com>
2026-09-24 14:58:49 +0530

    feat(media): add 90 degree geometry operations

--- src/main/java/io/synexia/chromellm/video/GeometryOperation.java
+++ src/main/java/io/synexia/chromellm/video/GeometryOperation.java
@@ -3,5 +3,7 @@ package io.synexia.chromellm.video;
 public enum GeometryOperation {
     FLIP_HORIZONTAL,
     FLIP_VERTICAL,
+    ROTATE_90_CLOCKWISE,
+    ROTATE_90_COUNTERCLOCKWISE,
     ROTATE_180
 }
