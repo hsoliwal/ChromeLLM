4867039ecc24f8dbade96306d81a88620350dd82
Hitesh Soliwal <hsoliwal@gmail.com>
2026-09-24 14:55:11 +0530

    feat(media): version media preset schema

--- src/main/java/io/synexia/chromellm/preset/MediaPreset.java
+++ src/main/java/io/synexia/chromellm/preset/MediaPreset.java
@@ -3,6 +3,7 @@ package io.synexia.chromellm.preset;
 import java.util.List;
 
 public record MediaPreset(
+        Integer version,
         String pipeline,
         LutPreset lut,
         List<OverlayPreset> overlays,
